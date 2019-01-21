2a476cf0cabfeea4c589e813c2045295e131d6dd
Ilya Kirilushkin <ilya.kirilushkin@zimad.com>
2019-01-21 18:01:33 +0300

    add login component

--- spacedust-frontend/src/app/app.module.ts
+++ spacedust-frontend/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import { SignupFormComponent } from './signup-form/signup-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    SignupFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
